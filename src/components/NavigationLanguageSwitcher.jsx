764733e70278acb2395fa3ffd09f226c4157f622
蒼時弦也 <elct9620@frost.tw>
2017-03-16 01:40:08 +0800

    Fix language switch append hash on url

--- src/components/NavigationLanguageSwitcher.jsx
+++ src/components/NavigationLanguageSwitcher.jsx
@@ -5,7 +5,8 @@ import { translate } from 'react-i18next';
 import i18n from '../i18n';
 
 const isEnglish = () => i18n.language === 'en';
-const ChangeLanguage = () => {
+const ChangeLanguage = (event) => {
+  event.preventDefault();
   if (isEnglish()) {
     i18n.changeLanguage('zh-TW');
   } else {
