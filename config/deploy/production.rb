1a61c9db97d5f72102d444e1bef703482e732b28
蒼時弦也 <elct9620@frost.tw>
2017-03-19 15:44:52 +0800

    Update production server location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 # TODO: Setup to use TGDF domain
-server "ec2-54-88-191-39.compute-1.amazonaws.com", user: "deploy", roles: %w{app db web}
+server "ec2-54-87-254-107.compute-1.amazonaws.com", user: "deploy", roles: %w{app db web}
