6cfcf1c53ddbd0a2c5527c040f5f0786c3f86a5a
Johnson Lin <arch.jslin@gmail.com>
2017-04-05 19:57:28 +0800

    try logo with css/div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,10 @@ import Navigation from './Navigation';
 import Banner from './Banner';
 import Introduce from './Introduce';
 
-import logo from './../assets/tgdf_logo_2017_web.jpg';
-
 const Header = ({ t }) => (
   <div id="header-wrapper">
     <header id="header">
-      <img src={logo} alt={"TGDF logo 2017"} />
+      <div id="logo"></div>
       <Navigation />
       <Route exact path="/" component={Banner} />
       <Route exact path="/" component={Introduce} />
