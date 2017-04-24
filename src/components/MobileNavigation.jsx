25496d23e3a167fe2b658d2ec3880c8950a78dfc
蒼時弦也 <elct9620@frost.tw>
2017-04-24 23:12:02 +0800

    Enable partner for mobile navigation

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -11,9 +11,8 @@ const Navigation = ({ t }) => (
         <NavigationItem exact to="/">{t('nav.home')}</NavigationItem>
         <NavigationItem to="/schedule">{t('nav.schedule')}</NavigationItem>
         <NavigationItem disabled to="/speakers">{t('nav.speakers')}</NavigationItem>
-        <NavigationItem disabled to="/partners">{t('nav.partners')}</NavigationItem>
+        <NavigationItem to="/partners">{t('nav.partners')}</NavigationItem>
         <NavigationItem disabled to="/register">{t('nav.register')}</NavigationItem>
-        <NavigationItem disabled to="/facebook">{t('nav.facebook')}</NavigationItem>
         <NavigationItem disabled to="/archive">{t('nav.archive')}</NavigationItem>
         <NavigationLanguageSwitcher />
       </ul>
