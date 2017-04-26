2a52e87aee446f499f3ef2f14e03b475d8ff8aa1
蒼時弦也 <elct9620@frost.tw>
2017-04-26 23:19:33 +0800

    Add slider to home page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import 'react-dom';
 import { translate } from 'react-i18next';
+import Slider from 'react-slick';
 
-const Banner = ({ t }) => (
+import slider1 from '../assets/slides/slide_1.jpg';
+import slider2 from '../assets/slides/slide_2.jpg';
+
+const Banner = () => (
   <section id="banner">
-    <header>
-      <h2>{t('home.cfp.call_for_paper')}</h2>
-      <p>{t('home.cfp.location')}</p>
-      <p>{t('home.cfp.date')}</p>
-      <div className="cfp__button">
-        <a className="button" href="http://igda-tw.kktix.cc/events/tgdf2017-cfp">{t('home.cfp.submit')}</a>
-      </div>
-    </header>
+    <Slider className="home-slider" autoplay arrows={false} dots={false} pauseOnHover={false}>
+      <img src={slider1} alt="" />
+      <img src={slider2} alt="" />
+    </Slider>
   </section>
 );
 
 Banner.propTypes = {
-  t: React.PropTypes.func.isRequired,
 };
 
 export default translate()(Banner);
