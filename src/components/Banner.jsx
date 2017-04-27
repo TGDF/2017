24370f05a03e64c1de020b69c1e67eadfbde8044
蒼時弦也 <elct9620@frost.tw>
2017-04-28 01:24:11 +0800

    Fix slider cannot loaded by babel

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,14 +3,11 @@ import 'react-dom';
 import { translate } from 'react-i18next';
 import Slider from 'react-slick';
 
-import slider1 from '../assets/slides/slide_1.jpg';
-import slider2 from '../assets/slides/slide_2.jpg';
-
 const Banner = () => (
   <section id="banner">
-    <Slider className="home-slider" autoplay arrows={false} dots={false} pauseOnHover={false}>
-      <img src={slider1} alt="" />
-      <img src={slider2} alt="" />
+    <Slider className="home-slider" arrows={false} dots={false} pauseOnHover={false}>
+      <div><div className="slider slider1" /></div>
+      <div><div className="slider slider2" /></div>
     </Slider>
   </section>
 );
